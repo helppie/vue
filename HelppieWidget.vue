ce4d8215420fcd2a95ac38ee309cc2c0fe06efcc
Arvind <heyarviind@gmail.com>
2020-10-23 21:57:59 +0530

    convert indent to spaces

--- HelppieWidget.vue
+++ HelppieWidget.vue
@@ -1,30 +1,29 @@
 <template>
-	<span open-helppie-widget>
-		<slot></slot>
-	</span>
+  <span open-helppie-widget>
+    <slot></slot>
+  </span>
 </template>
 
 <script>
-	/* eslint-disable */
 export default {
-	name: "HelppieWidget",
-	props: ["widgetId"],
-	created() {
-		!(function(e, t, p, a, i) {
-			(e.helppieData = e.helppieData || []),
-				(e.helppie = function() {
-					helppieData.push(arguments);
-				});
-			const n = t.createElement("script"),
-				c = t.getElementsByTagName("script")[0];
-			(n.type = "application/javascript"),
-				(n.src = "https://cdn.helppie.me/widget.js"),
-				(n.async = !0),
-				c.parentNode.insertBefore(n, c);
-		})(window, document);
-	},
-	mounted() {
-		helppie(this.widgetId);
-	},
+  name: "HelppieWidget",
+  props: ["widgetId"],
+  created() {
+    !(function(e, t, p, a, i) {
+      (e.helppieData = e.helppieData || []),
+        (e.helppie = function() {
+          helppieData.push(arguments);
+        });
+      const n = t.createElement("script"),
+        c = t.getElementsByTagName("script")[0];
+      (n.type = "application/javascript"),
+        (n.src = "https://cdn.helppie.me/widget.js"),
+        (n.async = !0),
+        c.parentNode.insertBefore(n, c);
+    })(window, document);
+  },
+  mounted() {
+    helppie(this.widgetId);
+  },
 };
 </script>
